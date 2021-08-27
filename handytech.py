f07093a41d747a73b3a4aa732a115bcac50b33ee
douglasmg7 <douglasmg7@gmail.com>
2021-08-27 17:03:29 -0300

    change shebang to python3

--- handytech.py
+++ handytech.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 import sys
 from logger import debug, info, warning, error, critical
 
--- process_xml.py
+++ process_xml.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 import os
 from logger import debug, info, warning, error, critical
 import xml.etree.ElementTree as ET
